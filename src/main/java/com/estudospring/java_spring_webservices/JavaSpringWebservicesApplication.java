88fca90f63ab54e683d0149ecbdd05c49d3b337e
cDorth <carloseduardodorth123@gmail.com>
2026-08-24 01:11:19 -0300

    first commit

---
+++ src/main/java/com/estudospring/java_spring_webservices/JavaSpringWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.estudospring.java_spring_webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudospring/java_spring_webservices/JavaSpringWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudospring.java_spring_webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
